883196ff93762aefab61cb59390a723dc7bd80ea
designens <inyoung524@naver.com>
2017-04-13 14:22:46 +0900

    가로 스크롤 배너 스크립트 오류 수정

--- markup/js/common.js
+++ markup/js/common.js
@@ -68,26 +68,13 @@
     });
 
     // 가로 스크롤 배너
-    // [참고] http://codepen.io/eldadfux/pen/zGpwdP
-    var element     = doc.getElementById('scrolling'),
-          wrapper     = doc.createElement('div'),
+    var element     = $('#scrolling'),
+          wrapper     = element.wrap('<div style="overflow: auto;">'),
 
           getCurrentFold = function(scroll, width) {
               return Math.ceil(width / scroll);
           },
 
           folds = Math.ceil(element.offsetWidth / wrapper.offsetWidth);
-
-      // Apply styles
-      wrapper.style.overflow = 'auto';
-
-      // Add 'virtual' DOM elements
-      element.parentNode.replaceChild(wrapper, element);
-      wrapper.appendChild(element);
-
-
-      wrapper.addEventListener('scroll', function() {
-          console.log(getCurrentFold(wrapper.scrollLeft + wrapper.offsetWidth, element.offsetWidth));
-      });
 
 })(window, document, window.jQuery);
